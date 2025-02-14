497c08ab7e40a0a23b61b2250869b7464325275a
Anton Standrik <astandrik@gmail.com>
2025-02-14 11:39:44 +0300

    fix: release

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -1,4 +1,4 @@
-import {Meta, StoryFn} from '@storybook/react';
+import type {Meta, StoryFn} from '@storybook/react';
 import React from 'react';
 
 import {FIXED, LEGACY_THEME, MOVING, YCLOUD_THEME} from '../lib/constants';
@@ -6,10 +6,12 @@ import {FIXED, LEGACY_THEME, MOVING, YCLOUD_THEME} from '../lib/constants';
 import ExampleDT100, {defaultProps} from './Example/Example';
 import {ResizeableTable} from './Resizeable/Resizeable';
 
-export default {
+const meta: Meta<typeof ExampleDT100> = {
     title: 'React Data Table',
     component: ExampleDT100,
-} as Meta<typeof ExampleDT100>;
+};
+
+export default meta;
 
 const templateArgTypes = {
     stickyHeadValues: {
@@ -27,12 +29,12 @@ const templateArgTypes = {
 
 const Template: StoryFn<typeof ExampleDT100> = (args) => <ExampleDT100 {...args} />;
 
-export const Default = Template.bind({});
+export const Default: StoryFn<typeof ExampleDT100> = Template.bind({});
 Default.args = defaultProps;
 Default.argTypes = templateArgTypes;
 
 const ResizeableTemplate: StoryFn<typeof ResizeableTable> = (args) => <ResizeableTable {...args} />;
 
-export const Resizeable = ResizeableTemplate.bind({});
-Default.args = {theme: YCLOUD_THEME};
-Default.argTypes = templateArgTypes;
+export const Resizeable: StoryFn<typeof ResizeableTable> = ResizeableTemplate.bind({});
+Resizeable.args = {theme: YCLOUD_THEME};
+Resizeable.argTypes = templateArgTypes;
